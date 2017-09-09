d228dce5e2b03826c214b917b624b6b84085893a
poornachandr <31505561+poornachandr@users.noreply.github.com>
2017-09-09 18:56:26 +0530

    Create new file

---
+++ largest among three numbers.c
@@ -0,0 +1,30 @@
+#includee<stdio.h>
+#include<conio.h>
+vvoid main()
+{
+ int a,b,c;
+ printf(/n"enter three numbers");
+ scanf("%d",&a,&b,&c);
+ if(a>b)
+ {
+  if(a>c)
+  {
+   printf(/n"a is larger");
+  }
+  else
+  {
+   printf(/n"c is larger");
+  }
+ }
+ else
+ {
+  if(c>b)
+  {
+   printf(/n"c is larger");
+  }
+  else
+  {
+   printf(/n"b is larger");
+  }
+ }
+ };
