ddd17f26020807a0a39b4d3ccfaa5c11e0a82494
poornachandr <31505561+poornachandr@users.noreply.github.com>
2017-09-13 18:56:11 +0530

    Create alphabet or vowel.c

---
+++ alphabet or vowel.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ char ch;
+ pintf(/n"Enter any character");
+ scanf("%c",&ch);
+ if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+ {
+  printf("Entered character is a vowel");
+ }
+ else
+ {
+  printf(/n"Entered character is consonant");
+ }
+ getch;
+ };
