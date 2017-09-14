6b01974c58aced1e0b84b87d74a45ef3f9c1af63
poornachandr <31505561+poornachandr@users.noreply.github.com>
2017-09-14 19:31:37 +0530

    Create alphabet or not.c

---
+++ alphabet or not.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{ 
+ char ch;
+ printf(/n"Enter a character");
+ scanf("%c",&ch);
+ if((ch>=65&&ch<=90)||(ch>=97&&ch<=122))
+ {
+  prinf(/n"character is alphabet");
+  }
+  else
+  {
+   printf(/n"character is not an alphabet");
+   }
+   getch;
+  }; 
