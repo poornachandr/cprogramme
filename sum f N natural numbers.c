e1453cd6406d831eb23b526bf0e7e7f126b32140
poornachandr <31505561+poornachandr@users.noreply.github.com>
2017-09-14 19:11:11 +0530

    Create sum f N natural numbers.c

---
+++ sum f N natural numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int n,i,sum=0;
+ printf(/n"Enter the number");
+ scanf("%d",&n);
+ for(i=1,i<=n,i++)
+ {
+  sum=sum+i;
+  }
+  printf(/n"sum is %d",sum);
+  };
